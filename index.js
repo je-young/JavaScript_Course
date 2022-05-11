44d246399b54e6b3f7fcf6d0d27801064d13d14a
je-young <jey2965@gmail.com>
2022-05-12 01:59:12 +0900

    변수(variables)

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-console.log("I like pizza!");
-console.log("It's really good!");
-window.alert("I REALLY LOVE PIZZA!");
+// variables
 
-// This is a comment
-/*
-  This
-  is
-  a
-  multiline
-  comment
-*/
+let firstName = 'Bro'; //string
+let age = 21 //number
+let student = true; //boolean
+
+
+console.log('Hello', firstName); //Hello Bro
+console.log('You are', age, 'years old'); //You are 21 years old
+console.log('Je-Young', student); //Je-Young true
+
+
+document.getElementById("p1").innerHTML = "Hello " + firstName; //Hello Bro
+document.getElementById("p2").innerHTML = "You are " + age + " years old"; //You are 21 years old
+document.getElementById("p3").innerHTML = "Enrolled: " + student; //Enrolled true
