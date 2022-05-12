269e11aa0f01615e3e9b77066f2a4f1412eb13e5
je-young <jey2965@gmail.com>
2022-05-12 16:34:59 +0900

    const 사용(실수로 인한 pi값 변경)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // const  = a variable that can't be changed(const = 변경할 수 없는 변수)
 
-let pi = 3.14159; //파이
+const PI = 3.14159; //const PI(대문자 좋은습관)
 let radius; //반지름
 let circumference;  //둘레
 
@@ -8,8 +8,8 @@ radius = window.prompt("원의 반지름을 입력"); //9를 입력함.
 //console.log(typeof radius); //string
 radius = Number(radius);  //타입을 Number로 변경
 
-pi = 420.69;  //파이 값을 실수로 변경시
+PI = 420.69;  //파이 값을 실수로 변경시 (위에서 이미 const 로 변경 할 수 없는 변수로 지정하여 오류남)
 
-circumference = 2 * pi * radius;  // 계산공식
+circumference = 2 * PI * radius;  // 계산공식
 
-console.log("둘레는:", circumference);  //실수로 인한 둘레는: 7572.42
+console.log("둘레는:", circumference);  //Uncaught TypeError: Assignment to constant variable.
