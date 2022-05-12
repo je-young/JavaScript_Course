43bb222e8620322e103ea2c07e0b473ce90fa0c8
je-young <jey2965@gmail.com>
2022-05-12 15:52:04 +0900

    type conversion

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-// How to accept user input(사용자 입력을 받아들이는 방법)
-
-// EASY WAY with a window prompt(창 프롬프트가 있는 쉬운 방법)
-
-// let usernames = window.prompt("성명?");
-// console.log(usernames);
+// Type Conversion = change the of a value to another(변환 유형 = 값을 다른 값으로 변경)
+//                   (strings, numbers, booleans) 
 
 
+/*
+let age = window.prompt("나이가 어떻게 되시나요?");
 
-// DIFFICULT WAY HTML textbox(어려운 방법 HTML 텍스트 상자)
+console.log(typeof age); //string
 
-let usernames = document.getElementById("myButton").onclick = function () {
-  
-  usernames = document.getElementById("myText").value;
-  console.log(usernames);
+age = Number(age); // age 변수를 넘버로 변경
 
-  document.getElementById("myLabel").innerHTML = "반가워요~ " + usernames + " 고객님!";
-}
+console.log(typeof age); //number
+
+age += 1;
+
+console.log("생일 축하 합니다! 당신의 나이는 " + age + " 살 입니다~^^");
+*/
+
+let w;
+let x;
+let y;
+let z;
+let s;
+
+w = Number("pizza"); //NaN 'number'
+x = Number("3.14"); //3.14 'number'
+y = String(3.14); //3.14 string
+z = Boolean(""); //false 'boolean'
+s = Boolean("pizza"); //true 'boolean'
+
+
+console.log(w, typeof w); //NaN 'number'
+console.log(x, typeof x); //3.14 'number'
+console.log(y, typeof y); //3.14 string
+console.log(z, typeof z); //false 'boolean'
+console.log(s, typeof s); //true 'boolean'
