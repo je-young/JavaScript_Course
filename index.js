f73b93a26090e7f0c4341a4c1c94246d6af156ec
je-young <jey2965@gmail.com>
2022-05-12 15:22:30 +0900

    user input(쉬운방법, 어려운방법)

--- index.js
+++ index.js
@@ -1,43 +1,18 @@
-/*
-  산술 표현식은 ...의 조합입니다.
-  피연산자 (값, 변수 등)
-  연산자( - * / %)
-  값으로 평가할 수 있는
-  ex. y = x + 5;
-*/
+// How to accept user input(사용자 입력을 받아들이는 방법)
 
-//let students = 20;
-//let students_2 = 21;
+// EASY WAY with a window prompt(창 프롬프트가 있는 쉬운 방법)
 
-//students = students + 1;  //21 더하기
-//students = students - 1;  //19 빼기
-//students = students * 2;  //40 곱하기
-//students = students / 2;  //10 나누기
-//let extraStudent = students % 3;  //2 나머지
-//let extraStudent = students % 2;  //0 나머지
-//let extraStudent_2 = students_2 % 3; //0 나머지
-
-//students += 1;  //21 일종의 바로가기
-//students -= 1;  //19
-//students *= 2;  //40
-//students /= 2; //10
-//let extraStudent %= 3;  //SyntaxError
+// let usernames = window.prompt("성명?");
+// console.log(usernames);
 
 
-//console.log(students);
-//console.log(extraStudent);
-//console.log(extraStudent_2);
 
-/*
-  연산자 우선 순위
-  1. perenthesis () //가로안의 수
-  2. exponents  //지수
-  3. multiplication & division  //곱하기 & 나누기
-  4. addition & subtraction //더하기 & 빼기
-*/
+// DIFFICULT WAY HTML textbox(어려운 방법 HTML 텍스트 상자)
 
-//let result = 1 + 2 * (3 + 4); //15
-//let result = (1 + 2) * (3 + 4); //21
-let result = (1 + 2) * 3 + 4; //13
+let usernames = document.getElementById("myButton").onclick = function () {
+  
+  usernames = document.getElementById("myText").value;
+  console.log(usernames);
 
-console.log(result);
+  document.getElementById("myLabel").innerHTML = "반가워요~ " + usernames + " 고객님!";
+}
