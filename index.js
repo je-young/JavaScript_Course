05add6ea697c8d8a4ccfb22e0bd13aa89e0aacee
je-young <jey2965@gmail.com>
2022-05-12 23:26:57 +0900

    카운트 프로그램(count +, -, reset button)

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-// hypotenuse calc practice program(빗변 계산 연습 프로그램)
+// count program(카운트 프로그램)
 
-let a;
-let b;
-let c;
+let count = 0;
 
-/*
-// 화면 입력
-a = window.prompt("A면 입력"); // string type
-a = Number(a);  // number type 으로 변경
+document.getElementById("decreaseBtn").onclick = function () {
+  count -= 1;
+  document.getElementById("countLabel").innerHTML = count;
+}
 
-b = window.prompt("B면 입력"); // string type
-b = Number(b);  // number type 으로 변경
+document.getElementById("resetBtn").onclick = function () {
+  count = 0;
+  document.getElementById("countLabel").innerHTML = count;
+}
 
-// 계산
-//c = Math.pow(a, 2) + Math.pow(b, 2);
-c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-console.log("측면 c:", c);
-*/
-
-document.getElementById("submitButton").onclick = function () {
-  a = document.getElementById("aTextBox").value;
-  a = Number(a);
-
-  b = document.getElementById("bTextBox").value;
-  b = Number(b);
-
-  c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
-
-  document.getElementById("cLabel").innerHTML = "C면 :" + c;
+document.getElementById("increaseBtn").onclick = function () {
+  count += 1;
+  document.getElementById("countLabel").innerHTML = count;
 }
