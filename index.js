3a2cd3202c8647de6355c46f2acbe2a4320d44fe
je-young <jey2965@gmail.com>
2022-05-13 00:12:39 +0900

    주사위 프로그램(random number generator)

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-// count program(카운트 프로그램)
+// random number generator(난수 생성기)
 
-let count = 0;
 
-document.getElementById("decreaseBtn").onclick = function () {
-  count -= 1;
-  document.getElementById("countLabel").innerHTML = count;
-}
+//let x = Math.random();
+//let x = Math.random() * 6;
+//let x = Math.floor(Math.random() * 6); //난수 0~5까지
 
-document.getElementById("resetBtn").onclick = function () {
-  count = 0;
-  document.getElementById("countLabel").innerHTML = count;
-}
+//주사위 3개
+// let x = Math.floor(Math.random() * 6) + 1; //난수 1~6까지(주사위)
+// let y = Math.floor(Math.random() * 6) + 1;
+// let z = Math.floor(Math.random() * 6) + 1;
 
-document.getElementById("increaseBtn").onclick = function () {
-  count += 1;
-  document.getElementById("countLabel").innerHTML = count;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+let x;
+let y;
+let z;
+
+document.getElementById("rollBtn").onclick = function () {
+
+  x = Math.floor(Math.random() * 6) + 1;
+  y = Math.floor(Math.random() * 6) + 1;
+  z = Math.floor(Math.random() * 6) + 1;
+
+  document.getElementById("xLabel").innerHTML = x;
+  document.getElementById("yLabel").innerHTML = y;
+  document.getElementById("zLabel").innerHTML = z;
 }
