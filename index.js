d3dc1c97ceccb71b26a4b0aaac919d92885c51ec
je-young <jey2965@gmail.com>
2022-05-12 17:50:07 +0900

    빗변 계산 연습 프로그램(1)

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-// Math 함수
-// Math 객체는 수학에서 자주 사용하는 상수와 함수들을 미리 구현해 놓은 자바스크립트 표준 내장 객체이다.
-// 생성자가 존재하지 않아서 따로 인스턴스를 생성하지 않더라도 Math 객체의 모든 method나 property를 바로 사용할 수 있다.
+// hypotenuse calc practice program(빗변 계산 연습 프로그램)
 
+let a;
+let b;
+let c;
 
-//let x = 3.14;
-let x;
-let y = 5;
-let z = 9;
-let maximum;
-let minimum;
+// 화면 입력
+a = window.prompt("A면 입력"); // string type
+a = Number(a);  // number type 으로 변경
 
+b = window.prompt("B면 입력"); // string type
+b = Number(b);  // number type 으로 변경
 
-//x = Math.round(x);  //3 소수점 첫 번째 자리에서 반올림 후 반환
-//x = Math.floor(x);  //3 인수와 같거나 작은 수 중에서 가장 큰 정수 반환
-//x = Math.ceil(x); //4 인수와 같거나 큰 수 중에서 가장 작은 정수 반환
-//x = Math.pow(x, 2); //9.8596 x의 y제곱을 반환
-//x = Math.sqrt(x); //1.772004514666935 x의 제곱근 반환
-//x = Math.abs(x);  //3.14 x의 절댓값 반환
-//maximum = Math.max(x, y, z);  //9 가장 큰 값 반환
-//minimum = Math.min(x, y, z);  //3.14 가장 작은 값 반환
-x = Math.PI;  //3.141592653589793 π 말그대로 PI 파이 원의 둘레와 지름의 비율=원주율  3.14159.... 의 값을 가짐 
+// 계산
+//c = Math.pow(a, 2) + Math.pow(b, 2);
+c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
-
-console.log(x);
-//console.log(maximum);
-//console.log(minimum);
+console.log("측면 c:", c);
