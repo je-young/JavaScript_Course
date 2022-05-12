e55d9cd5c64c8e4c6b9c42825771889ceea3640c
je-young <jey2965@gmail.com>
2022-05-12 16:27:24 +0900

    const 사용전(실수로 인한 pi값 변경)

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ let pi = 3.14159; //파이
 let radius; //반지름
 let circumference;  //둘레
 
-radius = window.prompt("원의 반지름을 입력");
+radius = window.prompt("원의 반지름을 입력"); //9를 입력함.
 //console.log(typeof radius); //string
 radius = Number(radius);  //타입을 Number로 변경
 
+pi = 420.69;  //파이 값을 실수로 변경시
+
 circumference = 2 * pi * radius;  // 계산공식
 
-console.log("둘레는:", circumference);  //둘레는: 56.54862
+console.log("둘레는:", circumference);  //실수로 인한 둘레는: 7572.42
