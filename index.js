22130bd44a04157e8c6a32469923186966cf6ba7
je-young <jey2965@gmail.com>
2022-05-12 16:36:19 +0900

    const 사용

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ radius = window.prompt("원의 반지름을 입력"); //9를 입력함.
 //console.log(typeof radius); //string
 radius = Number(radius);  //타입을 Number로 변경
 
-PI = 420.69;  //파이 값을 실수로 변경시 (위에서 이미 const 로 변경 할 수 없는 변수로 지정하여 오류남)
+//PI = 420.69;  //파이 값을 실수로 변경시 (위에서 이미 const 로 변경 할 수 없는 변수로 지정하여 오류남)
 
-circumference = 2 * PI * radius;  // 계산공식
+circumference = 2 * PI * radius;  //계산공식
 
-console.log("둘레는:", circumference);  //Uncaught TypeError: Assignment to constant variable.
+console.log("둘레는:", circumference);  //둘레는: 56.54862
