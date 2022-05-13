a84c2265e1498f18596cfc5a6b60f26865293abf
je-young <jey2965@gmail.com>
2022-05-13 13:49:46 +0900

    문자열 섹션 추출 slice(), indexOf()

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-// useful string properties & methods(유용한 문자열 속성 및 메서드)
-
-let userName = "Song Je Young";
-let poneNumber = "123-456-7890";
-
-//console.log(userName.length); //13
-//console.log(userName.charAt(0));  //s
-//console.log(userName.indexOf("o"))  //1
-//console.log(userName.lastIndexOf("o"))  //9
-//userName = userName.trim(); //공백제거
-//userName = userName.toUpperCase();  //모두 대문자
-//userName = userName.toLowerCase();  //모두 소문자
+// slice() 문자열의 섹션을 추출
+//         새 문자열로 반환합니다.
+//         원래 문자열을 수정하지 않고
 
 
-poneNumber = poneNumber.replaceAll("-", "/"); //모두 변경 
+let fullName = "Marlboro Gold";
+let firstName = "Song";
+let lastName = "Je Young";
 
-console.log(poneNumber);
+// 수동
+//firstName = fullName.slice(0, 4); //0번째 인덱스 부터 4인덱스 이전까지(Song)
+//lastName = fullName.slice(5);  //5번째 인덱스 부터 마지막까지(Je Young)
+
+// 자동
+firstName = fullName.slice(0, fullName.indexOf(" ")); //0번째 인덱스 부터 공백 이전까지
+lastName = fullName.slice(fullName.indexOf(" ") + 1); //첫번째 공백인덱스+1 부터 끝까지
+
+
+
+console.log(firstName);
+console.log(lastName);
