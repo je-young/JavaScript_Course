7701bd18f8ff243acbe37594312cb2e44d48bea4
je-young <jey2965@gmail.com>
2022-05-13 22:49:45 +0900

    !(NOT) 논리 연산자(true -> false , false -> true)

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-// && (논리 AND) 연산자 - 두조건이 모두 참 일경우 
-// || (논리 OR) 연산자 - 둘중 하나만 참 일경우
-// 자바스크립트에서의 &&와 || 연산자는 boolean 값이 아니라 그냥 값 자체를 리턴
+// !(NOT) 논리 연산자 NOT은 느낌표 !를 써서 만들 수 있습니다.
+// 피연산자를 불린형(true / false)으로 변환합니다.
+// true -> false , false -> true
 
 
-// let temp = 15;
-// if (temp >= 0 && temp <= 30) {
-//   console.log("날씨가 좋아요!");
-// }
-// else {
-//   console.log("날씨가 나빠요!");
-// }
-
-
-// let 온도 = 15;
-// if (온도 <= 0 || 온도 >= 30) {
-//   console.log("날씨가 나빠요!");
-// }
-// else {
-//   console.log("날씨가 좋아요!");
-// }
-
-
-let temp = 15;
+let temp = - 15;
 let sunny = true;
-if (temp > 0 && temp < 30 && sunny) {
-  console.log("날씨가 좋아요!");
+
+if (!(temp > 0)) {
+  console.log("밖은 춥다.");
 }
 else {
-  console.log("날씨가 나빠요!");
+  console.log("밖은 따뜻하다.");
+}
+if (!sunny) {
+  console.log("밖은 화창하다.");
+}
+else {
+  console.log("밖은 흐리다.");
 }
