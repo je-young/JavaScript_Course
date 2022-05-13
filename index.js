686528a7bb245ddc60a4f1a243b6d53dba4ef6d8
je-young <jey2965@gmail.com>
2022-05-13 15:06:22 +0900

    if문

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-// method chaining = calling one method after another
-//                   in one continuous line of code
-//                   (연속되는 한 줄의 암호로  메서드를 차례로 호출하는 방법)
+// if 문 = 의사 결정의 기본 형식
+//        조건이 참이면 무언가를 하십시오
+//        그렇지 않으면 하지 마십시오!
 
+/*
+let age = 100;
 
-let userNeme = "     Song Je Young      ";
+if (age >= 100) {
+  console.log("아직도 살아 있나요??")
+}
+else if (age >= 65) {
+  console.log("당신은 말년에 접어들었습니다.")
+}
+else if (age >= 18) {
+  console.log("당신은 성인 입니다.")
+}
+else if (age <= 0) {
+  console.log("당신은 아직 태어나지 않았습니다.")
+}
+else {
+  console.log("당신은 미성년자 입니다.")
+}
+*/
 
-// let 두줄코드 = userNeme.charAt(0);
-// 두줄코드 = 두줄코드.toUpperCase();
+let 접속상태 = true;
 
-let 한줄코드 = userNeme.toUpperCase().trim().charAt(0);
-// toUpperCase() = 대문자로 변경
-// trim() = 좌, 우 공백제거
-// charAt() = 인덱스에 위치한 단일문자 반환
-
-console.log(한줄코드);
+if (접속상태) {
+  console.log("온라인");
+}
+else {
+  console.log("오프라인");
+}
