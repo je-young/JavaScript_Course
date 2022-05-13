e34ede7c96f346e65eea2f55816a84129c6d0584
je-young <jey2965@gmail.com>
2022-05-13 22:10:44 +0900

    논리연산자 &&(AND) ||(OR)

--- index.js
+++ index.js
@@ -1,64 +1,31 @@
-// switch = 복수의 if 조건문은 switch문으로 바꿀 수 있습니다.
-//          switch문을 사용한 비교법은 특정 변수를 다양한 상황에서 비교할 수 있게 해줍니다.
-//          코드 자체가 비교 상황을 잘 설명한다는 장점도 있습니다.
-//          switch문은 하나 이상의 case문으로 구성됩니다. 대개 default문도 있지만, 이는 필수는 아닙니다.
+// && (논리 AND) 연산자 - 두조건이 모두 참 일경우 
+// || (논리 OR) 연산자 - 둘중 하나만 참 일경우
+// 자바스크립트에서의 &&와 || 연산자는 boolean 값이 아니라 그냥 값 자체를 리턴
 
-// switch
 
-let grade = "40";
-switch (true) {
-  case grade >= 89:
-    console.log("아주 잘 했어!");
-    break;
-  case grade >= 79:
-    console.log("잘했어!");
-    break;
-  case grade >= 69:
-    console.log("평균은 했어!");
-    break;
-  case grade > 59:
-    console.log("노력 좀 해봐!");
-    break;
-  case grade <= 59:
-    console.log("낙제!");
-    break;
-  default:
-    console.log(grade, "는 점수가 아닙니다!");
+// let temp = 15;
+// if (temp >= 0 && temp <= 30) {
+//   console.log("날씨가 좋아요!");
+// }
+// else {
+//   console.log("날씨가 나빠요!");
+// }
+
+
+// let 온도 = 15;
+// if (온도 <= 0 || 온도 >= 30) {
+//   console.log("날씨가 나빠요!");
+// }
+// else {
+//   console.log("날씨가 좋아요!");
+// }
+
+
+let temp = 15;
+let sunny = true;
+if (temp > 0 && temp < 30 && sunny) {
+  console.log("날씨가 좋아요!");
 }
-
-// let grade = "Pizza";
-// switch (grade) {
-//   case "A":
-//     console.log("아주 잘 했어!");
-//     break;
-//   case "B":
-//     console.log("잘했어!");
-//     break;
-//   case "C":
-//     console.log("평균은 했어!");
-//     break;
-//   case "D":
-//     console.log("노력 좀 해봐!");
-//     break;
-//   default:
-//     console.log(grade, "등급이 아닙니다!");
-// }
-
-
-
-// if
-// let grade = "";
-
-// if (grade == "A") {
-//   console.log("아주 잘 했어!");
-// } else if (grade == "B") {
-//   console.log("잘했어!");
-// } else if (grade == "C") {
-//   console.log("평균은 했어!");
-// } else if (grade == "D") {
-//   console.log("노력 좀 해봐!");
-// } else if (grade == "E") {
-//   console.log("커서 뭐 될래?");
-// } else {
-//   console.log(grade, "등급이 아닙니다.");
-// }
+else {
+  console.log("날씨가 나빠요!");
+}
