83182c8b8d4316498c5dff75f303a528fda881b4
je-young <jey2965@gmail.com>
2022-05-13 16:00:37 +0900

    checked 속성 (checkbox, radio button)

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-// if 문 = 의사 결정의 기본 형식
-//        조건이 참이면 무언가를 하십시오
-//        그렇지 않으면 하지 마십시오!
+// checked 속성
 
-/*
-let age = 100;
+document.getElementById("myButton").onclick = function () {
 
-if (age >= 100) {
-  console.log("아직도 살아 있나요??")
-}
-else if (age >= 65) {
-  console.log("당신은 말년에 접어들었습니다.")
-}
-else if (age >= 18) {
-  console.log("당신은 성인 입니다.")
-}
-else if (age <= 0) {
-  console.log("당신은 아직 태어나지 않았습니다.")
-}
-else {
-  console.log("당신은 미성년자 입니다.")
-}
-*/
+  const myCheckBox = document.getElementById("myCheckBox");
+  const bcBtn = document.getElementById("bcBtn");
+  const masterBtn = document.getElementById("masterBtn");
+  const visaBtn = document.getElementById("visaBtn");
 
-let 접속상태 = true;
+  if (myCheckBox.checked) {
+    console.log("당신은 구독 체크박스를 클릭하셨습니다.");
+  } else {
+    console.log("당신은 구독 체크박스를 클릭하세요.");
+  }
 
-if (접속상태) {
-  console.log("온라인");
-}
-else {
-  console.log("오프라인");
+  if (bcBtn.checked) {
+    console.log("BC카드로 결제합니다!")
+  } else if (masterBtn.checked) {
+    console.log("MASTR카드로 결제합니다!")
+  } else if (visaBtn.checked) {
+    console.log("VISA카드로 결제합니다!")
+  }else {
+    console.log("지불 수단을 선택하세요!")
+  }
+  
 }
