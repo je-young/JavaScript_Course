1fa961bdb2a47c6512337bca014f1e2636313f0d
je-young <jey2965@gmail.com>
2022-05-14 00:14:03 +0900

    do while loop

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-// while loop = while문은 조건문이 참일 때 실행되는 반복문이다. 조건은 문장안이 실행되기 전에 참, 거짓을 판단한다.
-// 코드 수행 순서는
-// 1. 먼저 조건식을 판단하고,
-// 2. 조건식의 결과가 true이면, 코드 블록 수행합니다.
-//    조건식의 결과가 false이면, while문을 빠져나옵니다.
-// 1번과 2번을 반복적으로 수행합니다.
+// do while loop = 코드 블록을 한 번 반복한 다음 조건이 true인 동안 반복합니다.
 
 
-let userName = "";
+let userName;
 
-while (userName == "" || userName == null) {
+/*
+while (userName == "") {
   userName = window.prompt("이름을 입력하세요.");
 }
+console.log("반갑습니다", userName); //반갑습니다 undefined
+*/
+
+// 위의 코드는 처음부터 false 이므로 코드를 최소 한번이상 실행하고 싶다면 do while 사용
+do {
+  userName = window.prompt("이름을 입력하세요.");
+}while (userName == "")
 
 console.log("반갑습니다", userName);
