cf93db1c8038c91fb10cb849922af219f6eb2639
je-young <jey2965@gmail.com>
2022-05-14 14:06:35 +0900

    for loop

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-// do while loop = 코드 블록을 한 번 반복한 다음 조건이 true인 동안 반복합니다.
+// for loop = 반복문 루프(Loop)는 코드 블록을 여러 번 실행 할 수 있게 해준다.
+// for - 코드 블록을 여러번 반복
+// for/in - 객체의 속성을 따라 반복
+// while - 지정된 조건이 true 인 동안 코드 블록을 반복
+// do/while - also l지정된 조건이 true 인 동안 코드 블록을 반복
+
+for (let counter = 1; counter <= 10; counter+=3){
+  console.log(counter);
+} // 1 ~ 10
 
 
-let userName;
-
-/*
-while (userName == "") {
-  userName = window.prompt("이름을 입력하세요.");
-}
-console.log("반갑습니다", userName); //반갑습니다 undefined
-*/
-
-// 위의 코드는 처음부터 false 이므로 코드를 최소 한번이상 실행하고 싶다면 do while 사용
-do {
-  userName = window.prompt("이름을 입력하세요.");
-}while (userName == "")
-
-console.log("반갑습니다", userName);
+for (let i = 10; i > 0; i -= 3) {
+  console.log(i);
+} // 10 ~ 1
+console.log("새해복 많이 받으세요!");
